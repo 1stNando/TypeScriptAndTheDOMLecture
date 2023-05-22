b9c07f055361dab64b1c6fffd29fe98f0e7b3754
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-05-22 17:09:48 -0400

    Added TS to handle clicking X on each square of the board.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import { setupCounter } from './counter'
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+//const firstListItem = document.querySelector('li')
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function handleClickSquare(event) {
+  const thingClickedOn = event.target
+
+  thingClickedOn.textContent = 'X'
+}
+
+//firstListItem?.addEventListener('click', handleClickSquare)
+
+// Select all squares to apply querySelector to all 'li' items
+const allSquares = document.querySelectorAll('li')
+//check to make sure the above worked
+console.log(allSquares)
+
+// now we use a forEach loop to addEventListener. Note we cannot add this via querySelectorAll() method. So we must do this instead
+allSquares.forEach((square) =>
+  square.addEventListener('click', handleClickSquare)
+)
